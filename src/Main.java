59010d4eb979e527b11821ff4f36c603b5dd6d65
stefanyds <ste.stefany22@gmail.com>
2023-11-23 18:58:59 -0300

    feat: class and objects

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,17 @@
 public class Main {
+    String name;
+
+    static String name2; //Static attribute
 
     public static void main(String[] args) {
-        //Casting is used to transform the type of a variable into another one.
-        int idade1 = 22;
-        double idade2 = idade1;
-        //^This works because double is bigger than integer^
+      System.out.println(name2);
+    }
 
-        idade1 = (int) idade2;
-
-        char letra = 'a';
-        String nome = String.valueOf(letra);
-        //^Char to String^
-        letra = nome.charAt(0);
-        //^String to Char^
-        String nome2 = String.valueOf(idade2);
-        //^Integer to String^
-        idade1 = Integer.parseInt(nome2);
-        //^String to Integer^
+    void declareName() {
+        name = "John";
+    }
+    
+    String getName() {
+        return name;
     }
 }
