bbbf3dd7771da2c939f746fa9b28ef2085863d77
stefanyds <ste.stefany22@gmail.com>
2023-11-23 19:50:23 -0300

    feat: working with access modifiers

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,10 @@ public class Main {
 
     static String name2; //Static attribute
 
+    protected int salary; //This variable is visible to Pessoa.java
+
+    private String lastName; //This variable is not visible to Pessoa.java
+
     public static void main(String[] args) {
       System.out.println(name2);
     }
---
+++ src/Pessoa.java
@@ -0,0 +1,8 @@
+public class Pessoa {
+    void createMain() {
+        Main myMain = new Main("John");
+        System.out.println(myMain.name);
+        System.out.println(myMain.salary);
+        System.out.println(Main.lastName);
+    }
+}
